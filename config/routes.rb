4f6e8acc7405210f74da9440384b5130ff932822
Muhidin <muhidinhukic1@gmail.com>
2020-12-04 15:49:03 -0600

    added favorite button

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
+  delete '/favorites/:id', to: 'favorites#destroy', as: 'unfavor'
+  post '/favorites/new', to: 'favorites#add_favorite', as: 'add_favorite'
+  
+
 end
