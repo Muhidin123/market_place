8d5172ab24251bc0a57cfae00aa8aee743ff4797
dszarkiel <d.szarkiel@gmail.com>
2020-12-08 10:17:06 -0600

    final version that is missing footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
     def index
         @items = Item.where.not(user_id: current_user.id)
-        bought_items = Transaction.where(user_id: current_user.id).map {|trans| trans.item}
+        bought_items = Transaction.all.map {|trans| trans.item}
         @items -= bought_items
     end
 end
